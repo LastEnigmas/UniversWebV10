4b9abb93971506171f3b0073adf8833aa72d020d
Mehrshad-w <microsoftmehr@gmail.com>
2022-09-23 00:50:47 +0330

    Update Signin

--- UniversWebV10/Program.cs
+++ UniversWebV10/Program.cs
@@ -42,8 +42,9 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthorization();
+app.UseAuthentication();
 
+app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{area=Main}/{controller=MainHome}/{action=Index}/{id?}");
